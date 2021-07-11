b74c30a84b624270d3ce51ab4bae09b8fb34300c
Awang Suria Trisakti <60491935+awgst@users.noreply.github.com>
2021-07-11 07:35:18 +0700

    Fix typo

--- resources/views/student/pendaftar.blade.php
+++ resources/views/student/pendaftar.blade.php
@@ -3,7 +3,7 @@
 @section('main')
     <div class="card">
         <div class="card-header">
-            Calon Peserta Didik Baru SMP N 1 Gabus
+            <h1>Calon Peserta Didik Baru SMP N 1 Gabus</h1>
         </div>
         <div class="card-content">
             <h2 style="text-align: center;">Selamat datang di PPDB Online SMP N 1 Gabus</h2>
