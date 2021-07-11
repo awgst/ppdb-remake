a16af410a46827d221d5ed475a7fdb992e278951
Awang Suria Trisakti <60491935+awgst@users.noreply.github.com>
2021-07-11 07:53:09 +0700

    Add bootstrap cdn

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -3,6 +3,9 @@
 <head>
 	<title>@yield('title')</title>
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<!-- jQuery -->
 	<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
 </head>
 <body>
@@ -12,6 +15,9 @@
 			<center>Copyright &copy 2020-<?=date('Y');?></center>
 		</footer>
 	</div>
+	<!-- Custom JS -->
 	<script src="{{ asset('js/script.js') }}"></script>
+	<!-- Bootstrap JS -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
