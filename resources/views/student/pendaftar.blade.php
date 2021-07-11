d4836f04e6bd5f0b8f3511171b6e7c9ad2613acb
Awang Suria Trisakti <60491935+awgst@users.noreply.github.com>
2021-07-11 07:29:18 +0700

    Create view for registrant

---
+++ resources/views/student/pendaftar.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.base')
+@section('title', 'Pendaftar Sementara')
+@section('main')
+    <div class="card">
+        <div class="card-header">
+            Calon Peserta Didik Baru SMP N 1 Gabus
+        </div>
+        <div class="card-content">
+            <h2 style="text-align: center;">Selamat datang di PPDB Online SMP N 1 Gabus</h2>
+            <p>Silahkan cari nama kamu dalam daftar berikut untuk mengetahui apakah kamu diterima atau tidak.</p>
+        </div>
+    </div>
+@endsection
